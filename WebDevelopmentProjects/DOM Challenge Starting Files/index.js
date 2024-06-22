2a11a62609b9b3dc02abafc5bc1c356ddb6715a7
MMyzell <MMyzell@protonmail.com>
2024-06-22 18:57:32 -0400

    2024-06-22---SavePoint

--- WebDevelopmentProjects/DOM Challenge Starting Files/index.js
+++ WebDevelopmentProjects/DOM Challenge Starting Files/index.js
@@ -1,1 +1,13 @@
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela";
+
+document.querySelector("button").style.backgroundColor = "yellow";
+
+document.querySelector("h1").classList.toggle("huge")
+
+
+
+
+
+
+
+
